5055b245c338937ca040dfaf53abc0d292b3108f
WillRRC <whartfiel@rrc.ca>
2024-01-31 16:45:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To run the app run this command
+
+docker run -p 7775:3000  whartfielrrc/my-react-app:1.0.2
+
+this will run it at port 7775 local host
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>coding 1</h1>
+      <h1>Will H</h1>
+    </div>
+  );
+}
+
+export default App;
